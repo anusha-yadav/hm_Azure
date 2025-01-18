a09673f0cf3f6b2beea508e0c07cc9a0a3ecbc4c
anusha-yadav <anushayadav.1904f1@gmail.com>
2025-01-18 15:52:23 +0530

    added keyvault

--- HM_Azure/Controllers/HomeController.cs
+++ HM_Azure/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using HM_Azure.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -15,6 +17,7 @@ namespace HM_Azure.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Message = TempData["Message"];
             return View();
         }
 
@@ -27,6 +30,31 @@ namespace HM_Azure.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void GetSecrets()
+        {
+            // Key Vault URI (replace with your Key Vault's URI)
+            string keyVaultUri = "https://dev-hm-1.vault.azure.net/";
+
+            // Create a SecretClient using DefaultAzureCredential
+            var client = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+            try
+            {
+                // Retrieve a secret by name
+                string secretName = "ConnectionStringDev"; // Replace with the name of your secret
+                KeyVaultSecret secret = client.GetSecret(secretName);
+
+                // Output the secret value
+                Console.WriteLine($"Secret Name: {secretName}");
+                Console.WriteLine($"Secret Value: {secret.Value}");
+                TempData["Message"] = secret.Value;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving secret: {ex.Message}");
+            }
         }
     }
 }
